1e289eefe9a21680604cd7d50b768715294968ad
Petko D. Petkov <pdp@websecurify.com>
2018-01-06 10:58:36 +0000

    Trying deployment script.

---
+++ .deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd build
+git init
+git config user.name "Travis-CI"
+git config user.email "travis-ci@gnucitizen.org"
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/gnucitizen/gnucitizen-blog" master:gh-pages > /dev/null 2>&1
